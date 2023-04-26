b9bdc2b80055f9b510d26ead1686179ddb8c3831
aaronchenwei <aaronchenwei@hotmail.com>
2023-04-26 10:42:34 +0800

    clean

--- mpi/quad/c/quad.c
+++ mpi/quad/c/quad.c
@@ -134,7 +134,7 @@ int main(int argc, char *argv[])
   */
   ierr = MPI_Finalize();
 
-  return 0;
+  return ierr;
 }
 /******************************************************************************/
 

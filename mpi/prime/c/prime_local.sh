3a0f873f0c9621928f19882658b908d16a759cdf
aaronchenwei <aaronchenwei@hotmail.com>
2023-04-28 16:59:03 +0800

    cmake

--- mpi/prime/c/prime_local.sh
+++ mpi/prime/c/prime_local.sh
@@ -2,16 +2,16 @@
 #
 #  Create new prime_local_output.txt, and write to it.
 #
-mpirun -np 1 ./prime > prime_local_output.txt
+mpirun -np 1 ./build/prime > prime_local_output.txt
 
 #
 #  Append to existing prime_local_output.txt.
 #
-mpirun -np 2 ./prime >> prime_local_output.txt
+mpirun -np 2 ./build/prime >> prime_local_output.txt
 
-mpirun -np 4 ./prime >> prime_local_output.txt
+mpirun -np 4 ./build/prime >> prime_local_output.txt
 
-mpirun -np 8 ./prime >> prime_local_output.txt
+mpirun -np 8 ./build/prime >> prime_local_output.txt
 
 #
 echo "prime_local: Normal end of execution."
